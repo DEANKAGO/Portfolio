e3fd7bbaa5836c64e57635a740a6a1c2b7e43c80
deankago <martokhago@gmail.com>
2024-04-19 06:54:52 -0400

    create home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,8 +4,24 @@ import { Navbar } from '../../components/Navbar/Navbar';
 
 export const Home = () => {
   return (
-    <div>
+    <div className='container'>
       <Navbar />
+      <div className='content'>
+        <div className='text'>
+          <h1 className=''>Welcome to my portfolio! I'm Martin Kago Muchai</h1>
+          <h1>A passionate software engineer</h1>
+          <p className='contentP'>
+            I specialize in full-stack development, mobile app development,
+            frontend development and backend development. My goal is to leverage
+            my technical expertise and problem-solving skills to deliver
+            innovative solutions that meet and exceed client expectations.
+          </p>
+          <button className='btnContact'>Contact</button>
+        </div>
+      </div>
+      <div className='homeImage'>
+        <img className='image' />
+      </div>
     </div>
   );
 };
