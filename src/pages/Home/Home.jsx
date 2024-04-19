0982e54d37add2cc83e65dbaf2407f4d5e2f99ef
deankago <martokhago@gmail.com>
2024-04-19 08:19:47 -0400

    style home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,19 +9,22 @@ export const Home = () => {
       <Navbar />
       <div className='content'>
         <div className='text'>
-          <h1 className=''>Welcome to my portfolio! I'm Martin Kago Muchai</h1>
-          <h1>A passionate software engineer</h1>
+          <h1 className='intro'>
+            Welcome to my portfolio! <br />
+            <span className='span'>I'm Martin Kago Muchai</span>
+          </h1>
+          <h1 className='intro'>A passionate software engineer</h1>
           <p className='contentP'>
             I specialize in full-stack development, mobile app development,
             frontend development and backend development. My goal is to leverage
             my technical expertise and problem-solving skills to deliver
             innovative solutions that meet and exceed client expectations.
           </p>
-          <button className='btnContact'>Contact</button>
+          <button className='btnContact'>Contact Me</button>
         </div>
-      </div>
-      <div className='homeImage'>
-        <img className='image' src={bgImage} />
+        <div className='homeImage'>
+          <img className='image' src={bgImage} />
+        </div>
       </div>
     </div>
   );
